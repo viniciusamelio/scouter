ef8bd77ee65106582c778d6c8b48b733f5104552
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-07-28 15:25:26 -0300

    feature: adding missing http verbs implementation

--- lib/src/domain/http_verbs.dart
+++ lib/src/domain/http_verbs.dart
@@ -23,3 +23,48 @@ class Post implements HttpVerb {
   @override
   final String verb = "post";
 }
+
+class Put implements HttpVerb {
+  Put(this.route);
+  @override
+  final String route;
+
+  @override
+  final String verb = "put";
+}
+
+class Patch implements HttpVerb {
+  Patch(this.route);
+  @override
+  final String route;
+
+  @override
+  final String verb = "patch";
+}
+
+class Delete implements HttpVerb {
+  Delete(this.route);
+  @override
+  final String route;
+
+  @override
+  final String verb = "delete";
+}
+
+class Options implements HttpVerb {
+  Options(this.route);
+  @override
+  final String route;
+
+  @override
+  final String verb = "options";
+}
+
+class Head implements HttpVerb {
+  Head(this.route);
+  @override
+  final String route;
+
+  @override
+  final String verb = "head";
+}

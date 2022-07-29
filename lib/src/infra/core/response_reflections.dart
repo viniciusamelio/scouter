6c5676264e01339c3e30c232ba023082ce1ba9df
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-07-29 10:42:26 -0300

    feature: response parse reflection

---
+++ lib/src/infra/core/response_reflections.dart
@@ -0,0 +1,27 @@
+import 'dart:mirrors';
+
+import 'package:scouter/src/domain/route.dart';
+
+abstract class ResponseReflections {
+  static HttpResponse getResponseFromObject(dynamic object) {
+    final Map<String, dynamic> responseBody = {};
+    final ref = reflect(object);
+
+    if (ref.type.reflectedType == HttpResponse) {
+      return object;
+    }
+
+    ref.type.declarations.forEach((_, member) {
+      if (member.owner!.simpleName == member.simpleName) {
+        return;
+      }
+      final value = ref.getField(_).reflectee;
+      final key = member.simpleName.toString().split('"')[1];
+      responseBody[key] = value;
+    });
+
+    return HttpResponse(
+      body: responseBody,
+    );
+  }
+}

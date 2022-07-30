bf63ca508b12d964d10f9111181b65bfdc12db57
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-07-30 11:14:22 -0300

    fix: removing dependencies from controller annotation

--- lib/src/domain/http_controller.dart
+++ lib/src/domain/http_controller.dart
@@ -1,14 +1,10 @@
 import 'package:scouter/src/domain/middleware.dart';
 
-import 'di.dart';
-
 class HttpController {
   const HttpController({
     this.name = "",
-    this.dependencies = const [],
     this.middlewares = const [],
   });
   final String? name;
-  final List<Injection> dependencies;
   final List<HttpMiddleware>? middlewares;
 }

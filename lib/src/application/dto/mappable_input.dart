acec0f9b26af3dea67c7d86f927c61ac7e9cf090
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-12-11 19:40:42 -0300

    doc: MappableInput class definition

--- lib/src/application/dto/mappable_input.dart
+++ lib/src/application/dto/mappable_input.dart
@@ -1,3 +1,7 @@
+/// It represents a DTO that will be parsed by your implementation.
+///
+/// It is recommended to use it when you need to receive a payload which may contains
+/// lists of other custom dtos.
 abstract class MappableInput {
   const MappableInput();
   MappableInput parse(dynamic map);
